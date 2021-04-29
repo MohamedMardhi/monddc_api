16e2f4cbbe776136599adcbf6b28b7a7d1ede9ac
sporodreki <mohamed.mardhi@gmail.com>
2021-04-30 00:39:07 +0200

    Add logs tools

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
+
 const express = require('express');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan');
+//var fs = require('fs')
+//var path = require('path')
 // Route Files
 const dossierdc = require('./routes/dossierdecompetences');
 
 dotenv.config({path: './config/config.env'});
 
 const app = express();
+
+// LOGS  MIDDLEWARE only on dev mode
+if(process.env.NODE_ENV === 'developement'){
+    app.use(morgan('dev'))
+    // app.use(morgan('common', {
+    //     stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+    // }))
+}
+
 
 app.use('/api/v1/dossierdc', dossierdc)
  
