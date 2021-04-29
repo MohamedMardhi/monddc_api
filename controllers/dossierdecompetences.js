94ad9ac118f4627c88f48dc45bbd8d08429c2190
sporodreki <mohamed.mardhi@gmail.com>
2021-04-29 07:33:57 +0200

    cleaner controller and routes

--- controllers/dossierdecompetences.js
+++ controllers/dossierdecompetences.js
@@ -5,8 +5,48 @@
 //@route GET /api/v1/dossierdc
 //@access Public
 
-exports.getDossierDC = (req, res, next) => {
+exports.getDossierDCs = (req, res, next) => {
     res
     .status(200)
     .json({success: true, msg: 'Get tous les dossiers de compétences'})
 }
+
+//@desc Get SINGLE dossier de compétences
+//@route GET /api/v1/dossierdc/:id
+//@access Public
+
+exports.getDossierDC = (req, res, next) => {
+    res
+    .status(200)
+    .json({success: true, msg: `Get un dossier de compétences ${req.params.id}`})
+}
+
+//@desc Create NEW dossier de compétences
+//@route POST /api/v1/dossierdc
+//@access Private
+
+exports.createDossierDC = (req, res, next) => {
+    res
+    .status(200)
+    .json({success: true, msg: `CREATE un dossier de compétences`})
+}
+
+//@desc UPDATE dossier de compétences
+//@route PUT /api/v1/dossierdc/:id
+//@access Private
+
+exports.updateDossierDC = (req, res, next) => {
+    res
+    .status(200)
+    .json({success: true, msg: `PUT un dossier de compétences ${req.params.id}`})
+}
+//@desc DELETE dossier de compétences
+//@route DELETE /api/v1/dossierdc/:id
+//@access Private
+
+exports.deleteDossierDC = (req, res, next) => {
+    res
+    .status(200)
+    .json({success: true, msg: `DELETE un dossier de compétences ${req.params.id}`})
+}
+
--- routes/dossierdecompetences.js
+++ routes/dossierdecompetences.js
@@ -1,25 +1,22 @@
 const express = require('express');
+const {
+    getDossierDC, 
+    getDossierDCs, 
+    createDossierDC, 
+    updateDossierDC, 
+    deleteDossierDC
+} = require('../controllers/dossierdecompetences')
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router
+    .route('/')
+    .get(getDossierDCs)
+    .post(createDossierDC)
 
-});
- 
-router.post('/', (req, res) => {
-    res
-    .status(200)
-    .json({success: true, msg: 'CREATE un dossier de compétences'})
-});
- 
-router.put('/:id', (req, res) => {
-    res
-    .status(200)
-    .json({success: true, msg: `PUT un dossier de compétences ${req.params.id}`})
-});
-router.delete('/:id', (req, res) => {
-    res
-    .status(200)
-    .json({success: true, msg: `DELETE un dossier de compétences ${req.params.id}`})
-});
+router
+    .route('/:id')
+    .get(getDossierDC)
+    .put(updateDossierDC)
+    .delete(deleteDossierDC)
 
 module.exports =  router;
